85128c621ce10b9a19d6caa16255e2d561cf357e
ariopri <arioprima1@gmail.com>
2022-11-24 23:28:38 +0700

    feat: create hero components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,10 @@
+import Hero from "./components/hero/Hero";
 import NavigationBar from "./components/navigationBar/NavigationBar";
 
 function App() {
   return <>
     <NavigationBar />
+    <Hero />
   </>;
 }
 
