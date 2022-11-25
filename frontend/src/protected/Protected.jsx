9dd7f037db2a831fb105cf7d9839746872b1f6ae
ariopri <arioprima1@gmail.com>
2022-11-25 13:09:33 +0700

    feat: create protected router

---
+++ frontend/src/protected/Protected.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Navigate, Outlet } from 'react-router-dom'
+import { Toast } from '../components/reusable/Toast'
+
+const Protected = () => {
+    let auth = { 'token': false }
+    return (
+        auth.token ? <Outlet /> : (Toast.fire({
+            icon: 'error',
+            title: 'You must be logged in to view this page'
+        })) && <Navigate to="/" />
+    )
+}
+
+export default Protected
